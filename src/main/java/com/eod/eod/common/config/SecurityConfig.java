e0fb234fd6e7bbd7eb47e5a733cfe317aeef43e0
randirao <randirao4245@gmail.com>
2025-12-22 19:26:16 +0900

    fix: 이상한거 수정

--- src/main/java/com/eod/eod/common/config/SecurityConfig.java
+++ src/main/java/com/eod/eod/common/config/SecurityConfig.java
@@ -87,8 +87,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/items/*/disposal-reason").permitAll()
 
                         // Claim 관련 API - 학생(USER)만 가능
-                        .requestMatchers(HttpMethod.POST, "/items/*/claim").hasRole("USER")
-                        .requestMatchers(HttpMethod.GET, "/items/claims/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/items/*/claims").hasRole("USER")
 
                         // 물품 등록/수정/삭제는 관리자만
                         .requestMatchers(HttpMethod.POST, "/items").hasRole("ADMIN")
