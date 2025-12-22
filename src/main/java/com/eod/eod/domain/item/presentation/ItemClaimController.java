4239a8dd973b9599e00139ba0d52d0260c13cd7c
randirao <randirao4245@gmail.com>
2025-12-22 19:23:31 +0900

    fix: /items/claims API SecurityConfig 권한 설정 수정

--- src/main/java/com/eod/eod/domain/item/presentation/ItemClaimController.java
+++ src/main/java/com/eod/eod/domain/item/presentation/ItemClaimController.java
@@ -191,10 +191,6 @@ public class ItemClaimController {
             @Parameter(hidden = true)
             @AuthenticationPrincipal User currentUser
     ) {
-        if (currentUser == null) {
-            throw new IllegalStateException("인증이 필요합니다.");
-        }
-
         if (!currentUser.isAdmin()) {
             throw new IllegalStateException("ADMIN 권한이 필요합니다.");
         }
